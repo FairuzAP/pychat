499f4f3ac7ba72fa0d962fee70efb1a6fea6c46b
13514104 <fairuz.pratama@tokopedia.com>
2018-04-10 18:07:43 +0700

    Fixed client to work on windows

--- pychat_client.py
+++ pychat_client.py
@@ -1,45 +1,54 @@
-import select, socket, sys
-from pychat_util import Room, Hall, Player
+import socket, sys, os, signal
+from threading import Thread
 import pychat_util
+
+
+def listen_to_server():
+    global msg_prefix
+    while True:
+        try:
+            msg = server_connection.recv(READ_BUFFER)
+        except:
+            os._exit(2)
+
+        if not msg:
+            print("Server down!")
+            os._exit(2)
+        else:
+            if msg == pychat_util.QUIT_STRING.encode():
+                sys.stdout.write('Bye\n')
+                server_connection.close()
+                os._exit(2)
+            else:
+                sys.stdout.write(msg.decode())
+                if 'Please tell us your name' in msg.decode():
+                    msg_prefix = 'name: '  # identifier for name
+                else:
+                    msg_prefix = ''
+                print('>', end=' ', flush=True)
+
+
+def listen_to_stdin():
+    while True:
+        msg = sys.stdin.readline()
+        msg = msg_prefix + msg
+        server_connection.sendall(msg.encode())
+
 
 READ_BUFFER = 4096
 
 if len(sys.argv) < 2:
-    print("Usage: Python3 client.py [hostname]", file = sys.stderr)
+    print("Usage: python client.py [hostname]", file = sys.stderr)
     sys.exit(1)
 else:
     server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_connection.connect((sys.argv[1], pychat_util.PORT))
 
-def prompt():
-    print('>', end=' ', flush = True)
-
 print("Connected to server\n")
 msg_prefix = ''
-
 socket_list = [sys.stdin, server_connection]
 
-while True:
-    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
-    for s in read_sockets:
-        if s is server_connection: # incoming message 
-            msg = s.recv(READ_BUFFER)
-            if not msg:
-                print("Server down!")
-                sys.exit(2)
-            else:
-                if msg == pychat_util.QUIT_STRING.encode():
-                    sys.stdout.write('Bye\n')
-                    sys.exit(2)
-                else:
-                    sys.stdout.write(msg.decode())
-                    if 'Please tell us your name' in msg.decode():
-                        msg_prefix = 'name: ' # identifier for name
-                    else:
-                        msg_prefix = ''
-                    prompt()
+Thread(target=listen_to_server).start()
+listen_to_stdin()
 
-        else:
-            msg = msg_prefix + sys.stdin.readline()
-            server_connection.sendall(msg.encode())
