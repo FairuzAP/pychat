d5551ccc1c7862896093005daeaa5d10a0f06c47
13514104 <fairuz.pratama@tokopedia.com>
2018-04-11 07:07:06 +0700

    Refactoring

--- pychat_client.py
+++ pychat_client.py
@@ -5,71 +5,71 @@ from Point import Point
 import pychat_util
 
 
-def listen_to_server():
-    global msg_prefix, shared_key
-    while True:
-        try:
-            msg = server_connection.recv(READ_BUFFER)
-        except:
-            os._exit(2)
+class PychatClient:
+    def __init__(self, host):
+        self.READ_BUFFER = 4096
 
-        if not msg:
-            print("Server down!")
+        self.server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_connection.connect((sys.argv[1], pychat_util.PORT))
+
+        print("Connected to server\n")
+        self.cipher = ECCipher(-1, 188, 7919, Point(224, 503), 20)
+        self.secret_key = self.cipher.gen_fancy_des_secret_key()
+        self.msg_prefix = ''
+        self.shared_key = None
+
+    def listen_to_server(self):
+        while True:
+            msg = self.server_connection.recv(self.READ_BUFFER)
+            if not msg:
+                print("Server down!")
+                os._exit(2)
+            else:
+                self.handle_server_msg(msg)
+
+    def handle_server_msg(self, msg):
+        if msg == pychat_util.QUIT_STRING.encode():
+            sys.stdout.write('Bye\n')
+            self.server_connection.close()
             os._exit(2)
 
         else:
-            if msg == pychat_util.QUIT_STRING.encode():
-                sys.stdout.write('Bye\n')
-                server_connection.close()
-                os._exit(2)
+            # If the server hasn't send it's partial key yet
+            if self.shared_key is None:
+                self.msg_prefix = 'name: '  # identifier for name
+                sys.stdout.write("Welcome to pychat.\nPlease tell us your name:\n")
+                self.shared_key = self.cipher.gen_fancy_des_shared_key(self.secret_key, msg)
 
             else:
-                # If the server hasn't send it's partial key yet
-                if shared_key is None:
-                    msg_prefix = 'name: '  # identifier for name
-                    sys.stdout.write("Welcome to pychat.\nPlease tell us your name:\n")
-                    shared_key = cipher.gen_fancy_des_shared_key(secret_key, msg)
+                # TODO: Decrypt the non-QUIT and PartialKey response here first
 
-                else:
-                    #TODO: Decrypt the non-QUIT and PartialKey response here first
+                decoded = msg.decode(encoding='UTF-8', errors='ignore')
+                self.msg_prefix = ''
+                sys.stdout.write(decoded)
 
-                    decoded = msg.decode(encoding='UTF-8', errors='ignore')
-                    msg_prefix = ''
-                    sys.stdout.write(decoded)
-                print('>', end=' ', flush=True)
+            print('>', end=' ', flush=True)
 
+    def listen_to_stdin(self):
+        while True:
+            msg = sys.stdin.readline()
+            msg = self.msg_prefix + msg
+            msg_bytes = msg.encode()
 
-def listen_to_stdin():
-    while True:
-        msg = sys.stdin.readline()
-        msg = msg_prefix + msg
-        msg_bytes = msg.encode()
+            # TODO: Encrypt the msg buffer here first, including the first username buffer
 
-        # TODO: Encrypt the msg buffer here first, including the first username buffer
+            # If this is the first message sent to server, prepend the partial key
+            if self.msg_prefix == 'name: ':
+                msg_bytes = bytes(self.cipher.gen_fancy_des_partial_key(self.secret_key)) + msg_bytes
 
-        # If this is the first message sent, include the partial key
-        if msg_prefix == 'name: ':
-            msg_bytes = bytes(cipher.gen_fancy_des_partial_key(secret_key)) + msg_bytes
+            self.server_connection.sendall(msg_bytes)
 
-        server_connection.sendall(msg_bytes)
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python client.py [hostname]", file=sys.stderr)
+        sys.exit(1)
 
-
-READ_BUFFER = 4096
-
-if len(sys.argv) < 2:
-    print("Usage: python client.py [hostname]", file = sys.stderr)
-    sys.exit(1)
-else:
-    server_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_connection.connect((sys.argv[1], pychat_util.PORT))
-
-print("Connected to server\n")
-cipher = ECCipher(-1, 188, 7919, Point(224, 503), 20)
-secret_key = cipher.gen_fancy_des_secret_key()
-msg_prefix = ''
-shared_key = None
-
-Thread(target=listen_to_server).start()
-listen_to_stdin()
-
+    else:
+        client = PychatClient(sys.argv[1])
+        Thread(target=client.listen_to_server).start()
+        client.listen_to_stdin()
--- pychat_server.py
+++ pychat_server.py
@@ -7,46 +7,57 @@ from ECC import ECCipher
 from Point import Point
 import pychat_util
 
-READ_BUFFER = 4096
 
-host = sys.argv[1] if len(sys.argv) >= 2 else ''
-listen_sock = pychat_util.create_socket((host, pychat_util.PORT))
+class PychatServer:
+    def __init__(self, host):
+        self.READ_BUFFER = 4096
+        self.listen_sock = pychat_util.create_socket((host, pychat_util.PORT))
 
-cipher = ECCipher(-1, 188, 7919, Point(224, 503), 20)
-secret_key = cipher.gen_fancy_des_secret_key()
+        self.cipher = ECCipher(-1, 188, 7919, Point(224, 503), 20)
+        self.secret_key = self.cipher.gen_fancy_des_secret_key()
 
-hall = Hall(cipher.gen_fancy_des_partial_key(secret_key))
-connection_list = []
-connection_list.append(listen_sock)
+        self.hall = Hall(self.cipher.gen_fancy_des_partial_key(self.secret_key))
+        self.connection_list = []
+        self.connection_list.append(self.listen_sock)
 
-while True:
-    # Player.fileno()
-    read_players, write_players, error_sockets = select.select(connection_list, [], [])
-    for player in read_players:
-        if player is listen_sock: # new connection, player is a socket
-            new_socket, add = player.accept()
-            new_player = Player(new_socket)
-            connection_list.append(new_player)
-            hall.welcome_new(new_player)
+    def start_listen(self):
+        while True:
+            read_players, write_players, error_sockets = select.select(self.connection_list, [], [])
+            for player in read_players:
+                if player is self.listen_sock:  # new connection, player is a socket
+                    self.handle_new_player(player)
 
-        else: # new message
-            msg = player.socket.recv(READ_BUFFER)
+                else:  # new message
+                    msg = player.socket.recv(self.READ_BUFFER)
+                    self.handle_player_msg(player, msg)
 
-            # If the player hasn't sent it's key and username yet,
-            if player.shared_key is None:
-                partial_key = msg[:msg.decode(errors='replace').rfind("name:")]
-                player.shared_key = cipher.gen_fancy_des_shared_key(secret_key, partial_key)
-                msg = msg[msg.decode(errors='replace').rfind("name:"):]
+            for sock in error_sockets:  # close error sockets
+                sock.close()
+                self.connection_list.remove(sock)
 
-            #TODO: Decrypt the msg buffer here first, including the first username buffer
+    def handle_new_player(self, player):
+        new_socket, add = player.accept()
+        new_player = Player(new_socket)
+        self.connection_list.append(new_player)
+        self.hall.welcome_new(new_player)
 
-            if msg:
-                msg = msg.decode(errors='replace').lower()
-                hall.handle_msg(player, msg)
-            else:
-                player.socket.close()
-                connection_list.remove(player)
+    def handle_player_msg(self, player, msg):
+        if player.shared_key is None: # If the player hasn't sent it's key and username yet,
+            partial_key = msg[:msg.decode(errors='replace').rfind("name:")]
+            player.shared_key = self.cipher.gen_fancy_des_shared_key(self.secret_key, partial_key)
+            msg = msg[msg.decode(errors='replace').rfind("name:"):]
 
-    for sock in error_sockets: # close error sockets
-        sock.close()
-        connection_list.remove(sock)
+        # TODO: Decrypt the msg buffer here first, including the first username buffer
+
+        if msg:
+            msg = msg.decode(errors='replace').lower()
+            self.hall.handle_msg(player, msg)
+        else:
+            player.socket.close()
+            self.connection_list.remove(player)
+
+
+if __name__ == '__main__':
+    host = sys.argv[1] if len(sys.argv) >= 2 else ''
+    server = PychatServer(host)
+    server.start_listen();
